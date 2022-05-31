1cc92b194c82262c68810d168eab9b5474d13237
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-05-31 09:15:40 +0200

    add FinishedTimerService

---
+++ services/timer/src/main/java/com/netsoft/android/timer/service/FinishedTimerService.kt
@@ -0,0 +1,11 @@
+package com.netsoft.android.timer.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class FinishedTimerService : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
