12ade93e0d731a59b385227859a7d4ad86201472
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-05-31 09:10:52 +0200

    parse time to HhMnSS

--- services/timer/src/main/java/com/netsoft/android/timer/extensions/TimerExtensions.kt
+++ services/timer/src/main/java/com/netsoft/android/timer/extensions/TimerExtensions.kt
@@ -6,6 +6,37 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import com.hubstaff.challenge.screen.main.MainActivity
+import com.netsoft.android.timer.common.ZERO_STRING
+import java.math.BigDecimal
+import kotlin.math.absoluteValue
+
+fun Float.roundUp(): Long = this.toBigDecimal().setScale(0, BigDecimal.ROUND_UP).longValueExact()
+
+fun Int.toStringOrEmpty(): String = if (this.isZero()) EMPTY else this.toString()
+fun Int.toFormattedString(): String {
+    return if (absoluteValue in 9 downTo 0) "$ZERO_STRING$absoluteValue" else absoluteValue.toStringOrEmpty()
+}
+
+fun Int.minuteToString(hasHour: Boolean): String =
+    if (hasHour) this.toFormattedString() else this.toStringOrEmpty()
+
+fun Int.secondToString(hasMinute: Boolean): String =
+    if (hasMinute) this.toFormattedString() else this.toString()
+
+fun String.removeExtraColon(): String =
+    if (this.first().toString() == COLON) takeLast(length - 1) else this
+
+fun Long.toHhMmSs(): String {
+    val hours = ((this / (1000 * 60 * 60) % 24)).toInt().toStringOrEmpty()
+    val minutes = ((this / (1000 * 60) % 60)).toInt().minuteToString(hours.isNotEmpty())
+    val seconds = ((this / 1000) % 60).toInt().secondToString(minutes.isNotEmpty())
+    var formattedTime = "$hours$COLON$minutes$COLON$seconds"
+    while (formattedTime.isNotEmpty() && formattedTime.first().toString() == COLON) {
+        formattedTime = formattedTime.removeExtraColon()
+    }
+    return formattedTime
+}
+
 
 fun isOreoPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
 
