bb055f368c0663bc298270374f17d424b4140c83
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-05-31 09:13:01 +0200

    update TimerExtensions

--- services/timer/src/main/java/com/netsoft/android/timer/extensions/TimerExtensions.kt
+++ services/timer/src/main/java/com/netsoft/android/timer/extensions/TimerExtensions.kt
@@ -5,10 +5,23 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import com.hubstaff.challenge.screen.main.MainActivity
-import com.netsoft.android.timer.common.ZERO_STRING
+import com.netsoft.android.timer.common.*
 import java.math.BigDecimal
+import java.util.concurrent.TimeUnit
 import kotlin.math.absoluteValue
+
+
+fun String.fillWithZeros() = this.padStart(MAX_LENGTH_TIMER, ZERO_STRING.first())
+fun String.removeLast() = if (isNotEmpty()) this.take(this.length - 1) else this
+fun String.firstInputIsZero(input: String) = this.isEmpty() && input == ZERO_STRING
+fun Long.isNotZero(): Boolean = this != ZERO_LONG
+fun Long?.getPositiveValue(): Long = this?.let { if (this < 0) ZERO_LONG else this } ?: ZERO_LONG
+fun Long.isZero(): Boolean = this == ZERO_LONG
+fun Int.isZero(): Boolean = this == ZERO_INT
+
 
 fun Float.roundUp(): Long = this.toBigDecimal().setScale(0, BigDecimal.ROUND_UP).longValueExact()
 
@@ -37,6 +50,14 @@ fun Long.toHhMmSs(): String {
     return formattedTime
 }
 
+fun String.calculateFontSize(): TextUnit {
+    return when (length) {
+        8 -> 40.sp
+        7 -> 48.sp
+        5 -> 64.sp
+        else -> 72.sp
+    }
+}
 
 fun isOreoPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
 
