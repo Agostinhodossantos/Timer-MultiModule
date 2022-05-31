4757376855e76a4dd8a31fd4a3945ca109576525
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-05-31 09:46:41 +0200

    provide TimerManager

--- services/timer/src/main/java/com/netsoft/android/timer/di/AppModule.kt
+++ services/timer/src/main/java/com/netsoft/android/timer/di/AppModule.kt
@@ -1,4 +1,20 @@
 package com.netsoft.android.timer.di
 
-class AppModule {
+import com.netsoft.android.timer.countdown.IntermittentTimerManager
+import com.netsoft.android.timer.countdown.IntermittentTimerManagerImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideTimerManager(): IntermittentTimerManager {
+        return IntermittentTimerManagerImpl()
+    }
 }
