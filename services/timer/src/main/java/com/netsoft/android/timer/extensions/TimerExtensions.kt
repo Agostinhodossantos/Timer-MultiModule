810ddf327964db792ff1be40deefbca2c52b6c2d
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-05-31 09:07:37 +0200

    create NotificationBuilder

--- services/timer/src/main/java/com/netsoft/android/timer/extensions/TimerExtensions.kt
+++ services/timer/src/main/java/com/netsoft/android/timer/extensions/TimerExtensions.kt
@@ -1,5 +1,19 @@
 package com.netsoft.android.timer.extensions
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Build
+import com.hubstaff.challenge.screen.main.MainActivity
 
 fun isOreoPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+
+fun Context.getLaunchIntent() =
+    packageManager.getLaunchIntentForPackage("com.ericktijerou.jettimer")
+
+@SuppressLint("UnspecifiedImmutableFlag")
+fun Context.getOpenTimerTabIntent(): PendingIntent {
+    val intent = getLaunchIntent() ?: Intent(this, MainActivity::class.java)
+    return PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+}
--- services/timer/src/main/java/com/netsoft/android/timer/service/TimerService.kt
+++ services/timer/src/main/java/com/netsoft/android/timer/service/TimerService.kt
@@ -1,15 +1,25 @@
 package com.netsoft.android.timer.service
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.netsoft.android.timer.R
+import com.netsoft.android.timer.TimerState
+import com.netsoft.android.timer.common.ONE_THOUSAND_INT
 import com.netsoft.android.timer.common.TIMER_RUNNING_ID
+import com.netsoft.android.timer.common.ZERO_LONG
+import com.netsoft.android.timer.extensions.getOpenTimerTabIntent
 import com.netsoft.android.timer.extensions.isOreoPlus
 import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 class TimerService : Service() {
-
     private val bus = EventBus.getDefault()
 
     override fun onCreate() {
@@ -21,23 +31,76 @@ class TimerService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
-        //startForeground(TIMER_RUNNING_ID, notification(""))
+        startForeground(TIMER_RUNNING_ID, notification(""))
         return START_NOT_STICKY
     }
 
-    fun Context.startTimerService() {
-        if (isOreoPlus()) {
-            startForegroundService(Intent(this, TimerService::class.java))
-        } else {
-            startService(Intent(this, TimerService::class.java))
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onMessageEvent(state: TimerState.Running) {
+        if (state.tick % ONE_THOUSAND_INT == ZERO_LONG) {
+            val notification: Notification = notification(state.tick.toString()) //TODO parse toHhMnSs
+            val mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            mNotificationManager.notify(TIMER_RUNNING_ID, notification)
         }
     }
 
-    fun Context.stopTimerService() {
-        EventBus.getDefault().post(TimerStopService)
-        stopService(Intent(this, TimerService::class.java))
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onMessageEvent(event: TimerStopService) {
+        stopService()
     }
 
-    object TimerStopService
+    private fun stopService() {
+        if (isOreoPlus()) {
+            stopForeground(true)
+        } else {
+            stopSelf()
+        }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        bus.unregister(this)
+    }
+
+    private fun notification(formattedTick: String): Notification {
+        val channelId = "simple_alarm_timer"
+        val label = "HSChallenge"
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (isOreoPlus()) {
+            val importance = NotificationManager.IMPORTANCE_LOW
+            NotificationChannel(channelId, label, importance).apply {
+                setSound(null, null)
+                notificationManager.createNotificationChannel(this)
+            }
+        }
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(formattedTick)
+            .setContentText(getString(R.string.label_timer))
+            .setSmallIcon(R.drawable.ic_logo)
+            .setContentIntent(this.getOpenTimerTabIntent())
+            .setSound(null)
+            .setOngoing(true)
+            .setAutoCancel(true)
+            .setChannelId(channelId)
+
+        builder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+        return builder.build()
+    }
 }
+
+fun Context.startTimerService() {
+    if (isOreoPlus()) {
+        startForegroundService(Intent(this, TimerService::class.java))
+    } else {
+        startService(Intent(this, TimerService::class.java))
+    }
+}
+
+fun Context.stopTimerService() {
+    EventBus.getDefault().post(TimerStopService)
+    stopService(Intent(this, TimerService::class.java))
+}
+
+object TimerStopService
