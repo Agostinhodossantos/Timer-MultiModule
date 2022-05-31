2f126e0cd3ffe2a778c984437607188f4cacc556
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-05-31 09:27:01 +0200

    add NotificationReceiver

---
+++ services/timer/src/main/java/com/netsoft/android/timer/broadcast/NotificationReceiver.kt
@@ -0,0 +1,15 @@
+package com.netsoft.android.timer.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.netsoft.android.timer.TimerState
+import org.greenrobot.eventbus.EventBus
+
+class NotificationReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        EventBus.getDefault().post(TimerState.Finish)
+        EventBus.getDefault().post(TimerState.Finished)
+    }
+}
--- services/timer/src/main/java/com/netsoft/android/timer/service/FinishedTimerService.kt
+++ services/timer/src/main/java/com/netsoft/android/timer/service/FinishedTimerService.kt
@@ -1,11 +1,116 @@
 package com.netsoft.android.timer.service
 
-import android.app.Service
+import android.annotation.SuppressLint
+import android.app.*
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.netsoft.android.timer.R
+import com.netsoft.android.timer.TimerState
+import com.netsoft.android.timer.common.*
+import com.netsoft.android.timer.extensions.getOpenTimerTabIntent
+import com.netsoft.android.timer.extensions.isOreoPlus
+import com.netsoft.android.timer.extensions.toHhMmSs
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+import javax.inject.Inject
 
 class FinishedTimerService : Service() {
-    override fun onBind(p0: Intent?): IBinder? {
-        TODO("Not yet implemented")
+
+    private val bus = EventBus.getDefault()
+
+    override fun onCreate() {
+        super.onCreate()
+        bus.register(this)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        startForeground(TIMER_FINISH_RUNNING_ID, notification(ZERO_STRING))
+        return START_NOT_STICKY
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onMessageEvent(state: TimerState.Running) {
+        if (state.tick % ONE_THOUSAND_INT == ZERO_LONG) {
+            val notification: Notification = notification(state.tick.toHhMmSs())
+            val mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            mNotificationManager.notify(TIMER_FINISH_RUNNING_ID, notification)
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onMessageEvent(event: FinishedTimerStopService) {
+        stopService()
+    }
+
+    private fun stopService() {
+        if (isOreoPlus()) {
+            stopForeground(true)
+        } else {
+            stopSelf()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bus.unregister(this)
+    }
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun notification(formattedTick: String): Notification {
+        val channelId = "finished_alarm_timer"
+        val label = getString(R.string.app_name)
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (isOreoPlus()) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            NotificationChannel(channelId, label, importance).apply {
+                setSound(null, null)
+                notificationManager.createNotificationChannel(this)
+            }
+        }
+        val broadcastIntent = Intent(application, NotificationReceiver::class.java)
+        val broadcastPendingIntent: PendingIntent =
+            PendingIntent.getBroadcast(
+                application,
+                0,
+                broadcastIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(formattedTick)
+            .setContentText(getString(R.string.label_time_up))
+            .setSmallIcon(R.drawable.ic_logo)
+            .setContentIntent(this.getOpenTimerTabIntent())
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setSound(null)
+            .setOngoing(true)
+            .setAutoCancel(true)
+            .setChannelId(channelId)
+            .addAction(R.drawable.ic_logo, getString(R.string.label_stop), broadcastPendingIntent)
+        builder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+        return builder.build()
     }
 }
+
+fun Context.startFinishedTimerService() {
+    if (isOreoPlus()) {
+        startForegroundService(Intent(this, FinishedTimerService::class.java))
+    } else {
+        startService(Intent(this, FinishedTimerService::class.java))
+    }
+}
+
+fun Context.stopFinishedTimerService() {
+    EventBus.getDefault().post(FinishedTimerStopService)
+    stopService(Intent(this, FinishedTimerService::class.java))
+}
+
+object FinishedTimerStopService
