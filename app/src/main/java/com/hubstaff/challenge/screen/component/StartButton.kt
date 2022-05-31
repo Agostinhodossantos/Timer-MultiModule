5c1bb485cdca4de87884e6c320ccd1b6bd68b9b7
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-05-31 11:59:10 +0200

    add StartButton component

---
+++ app/src/main/java/com/hubstaff/challenge/screen/component/StartButton.kt
@@ -0,0 +1,50 @@
+package com.hubstaff.challenge.screen.component
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.hubstaff.challenge.R
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun StartButton(visible: Boolean, modifier: Modifier, onClick: () -> Unit) {
+    BoxWithConstraints(contentAlignment = Alignment.BottomCenter, modifier = modifier) {
+        val height = with(LocalDensity.current) { maxHeight.toPx().toInt() }
+        AnimatedVisibility(
+            visible = visible,
+            enter = slideInVertically(initialOffsetY = { height }),
+            exit = slideOutVertically(targetOffsetY = { height })
+        ) {
+            IconButton(
+                onClick = onClick,
+                modifier = Modifier
+                    .size(56.dp)
+                    .background(
+                        color = MaterialTheme.colors.secondaryVariant,
+                        shape = CircleShape
+                    )
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.PlayArrow,
+                    contentDescription = stringResource(R.string.label_start),
+                )
+            }
+        }
+    }
+}
--- common/theme/src/main/java/com/hubstaff/theme/Theme.kt
+++ common/theme/src/main/java/com/hubstaff/theme/Theme.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
@@ -47,4 +47,33 @@ fun HubstaffAppTheme(
         shapes = Shapes,
         content = content
     )
+}
+
+
+@Stable
+class HubstaffColors(
+    textPrimaryColor: Color,
+    textSecondaryColor: Color,
+    searchBoxColor: Color,
+    textVariantColor: Color,
+    isDark: Boolean
+) {
+    var textPrimaryColor by mutableStateOf(textPrimaryColor)
+        private set
+    var textSecondaryColor by mutableStateOf(textSecondaryColor)
+        private set
+    var searchBoxColor by mutableStateOf(searchBoxColor)
+        private set
+    var textVariantColor by mutableStateOf(textVariantColor)
+        private set
+    var isDark by mutableStateOf(isDark)
+        private set
+
+    fun update(other: HubstaffColors) {
+        textPrimaryColor = other.textPrimaryColor
+        textSecondaryColor = other.textSecondaryColor
+        searchBoxColor = other.searchBoxColor
+        textVariantColor = other.textVariantColor
+        isDark = other.isDark
+    }
 }
